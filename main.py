4fedf5fc1b660c24027764f0b4e9a471f61203bc
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-07-12 21:30:43 +0530

    feat: add documentation for api

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
-app = FastAPI()
+app = FastAPI(
+    title="Mahendran Murugan Learning FastAPI",
+    description="To test fastapi implementation and decide it is suitable for projects",
+    version="0.0.1"
+)
 
 class Details(BaseModel):
-    name: str
-    age: int
-    status: str
+    """Basic Syntax of Details
+
+    Args:
+        BaseModel (_type_): _description_
+    """
+    name: str = Field(description="Name of a Person")
+    age: int = Field(description="Age of a Person")
+    status: str = Field(description="Status of a Person")
     
-@app.get("/")
+@app.get("/",
+         responses={
+             404: {"description":"Person not Found"},
+             400: {"description":"Error Occured"}
+         })
 def getDetails():
     return Details(name="Mahendran", age=19, status="Single")
